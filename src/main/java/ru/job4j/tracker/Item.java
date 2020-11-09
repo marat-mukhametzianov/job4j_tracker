31f178bce99eda1e85e5ed9193453775b4c318c9
marat-mukhametzianov <marat.mukhametzianov@yahoo.com>
2020-11-09 09:23:44 -0500

    2. Перегрузка конструктора [#389735]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -7,6 +7,17 @@ public class Item {
     private String name;
     private LocalDateTime dateTime = LocalDateTime.now();
 
+    public Item() {}
+
+    public Item(int id) {
+        this.id = id;
+    }
+
+    public Item(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
     public LocalDateTime getCreated() {return dateTime;}
 
     public int getId() {
