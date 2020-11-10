fa74ca81837cad938f86f3c5491bcb78f2646889
marat-mukhametzianov <marat.mukhametzianov@yahoo.com>
2020-11-10 18:06:51 -0500

    4. Переопределение [#389736]

--- src/main/java/ru/job4j/oop/inheritance/report/JSONReport.java
+++ src/main/java/ru/job4j/oop/inheritance/report/JSONReport.java
@@ -3,6 +3,9 @@ package ru.job4j.oop.inheritance.report;
 public class JSONReport extends TextReport {
     @Override
     public String generate(String name, String body) {
-        return "name : " + name + System.lineSeparator() + "body : " + body;
+        return "{" + System.lineSeparator() +
+                "\tname : " + name + "," + System.lineSeparator() +
+                "\tbody : " + body + System.lineSeparator() +
+                "}";
     }
 }
