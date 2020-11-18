6194ff74128aea34f6428d9d0ebf76d4d5ddacc7
marat-mukhametzianov <marat.mukhametzianov@yahoo.com>
2020-11-17 20:28:32 -0500

    2. Scanner и чтение числа из консоли. [#389672]

--- src/main/java/ru/job4j/io/Matches.java
+++ src/main/java/ru/job4j/io/Matches.java
@@ -4,45 +4,52 @@ import java.util.Scanner;
 
 public class Matches {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
         boolean running = true;
-        byte matchQuantity = 11;
-        System.out.printf("There are %d matches on the table.\n", matchQuantity);
+        byte matchesRemain = 11;
+        System.out.printf("There are %d matches on the table.\n", matchesRemain);
         while (running) {
-            /**
-             * Цикл, который крутится до тех пор, пока первый игрок не возмет правильное количество спичек
-             */
-            byte picked;
-            boolean takingIsCorrect;
-            do {
-                System.out.printf("How many matches pick the first player up?\n");
-                picked = Byte.valueOf(input.nextLine());
-                takingIsCorrect = picked > 3 || picked < 1;
-                if (takingIsCorrect) {
-                    System.out.println("Wrong quantity! Try again!");
-                }
-            } while (takingIsCorrect);
-            matchQuantity = (byte) (matchQuantity - picked) < 0 ? 0 : (byte) (matchQuantity - picked);
-            System.out.printf("There are %d matches on the table.\n", matchQuantity);
-            if (matchQuantity == 0) {
+            byte picked = getPicked("How many matches pick the first player up?\n");
+            matchesRemain = matchesRemain(matchesRemain, picked);
+
+            if (matchesRemain == 0) {
                 running = false;
-                System.out.printf("The player 1 have won!");
+                running = checkWin(matchesRemain, running, "first");
             }else {
-                do{
-                    System.out.printf("How many matches pick the second player up?\n");
-                    picked = Byte.valueOf(input.nextLine());
-                    takingIsCorrect = picked > 3 || picked < 1;
-                    if (takingIsCorrect) {
-                        System.out.println("Wrong quantity! Try again!");
-                    }
-                } while (takingIsCorrect);
-                matchQuantity = (byte) (matchQuantity - picked) < 0 ? 0 : (byte) (matchQuantity - picked);
-                System.out.printf("There are %d matches on the table.\n", matchQuantity);
-                if (matchQuantity == 0) {
-                    running = false;
-                    System.out.printf("The player 2 have won!");
-                }
+                picked = getPicked("How many matches pick the second player up?\n");
+                matchesRemain = matchesRemain(matchesRemain, picked);
+                running = checkWin(matchesRemain, running, "second");
             }
         }
     }
+
+    private static boolean checkWin(byte matchesRemain, boolean running, String whoWon) {
+        if (matchesRemain == 0) {
+            running = false;
+            System.out.printf("The player %s have won!", whoWon);
+        }
+        return running;
+    }
+
+    private static byte matchesRemain(byte matchesRemain, byte picked) {
+        matchesRemain = (byte) (matchesRemain - picked) < 0 ? 0 : (byte) (matchesRemain - picked);
+        System.out.printf("There are %d matches on the table.\n", matchesRemain);
+        return matchesRemain;
+    }
+
+    private static byte getPicked(String prompt) {
+        byte picked;
+        boolean takingIsCorrect;
+        Scanner input = new Scanner(System.in);
+        do {
+            System.out.printf(prompt);
+            picked = Byte.valueOf(input.nextLine());
+            takingIsCorrect = picked > 3 || picked < 1;
+            if (takingIsCorrect) {
+                System.out.println("Wrong quantity! Try again!");
+            }
+        } while (takingIsCorrect);
+        return picked;
+    }
+
+
 }
