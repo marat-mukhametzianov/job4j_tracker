113c0bd371f0eb26d751d0243ba4c154d5ad5974
marat-mukhametzianov <marat.mukhametzianov@yahoo.com>
2020-11-24 21:53:14 -0500

    9. Написать тесты на StartUI. [#389684]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -2,9 +2,7 @@ package ru.job4j.tracker;
 
 import org.junit.Assert;
 import org.junit.Test;
-import ru.job4j.tracker.actions.CreateAction;
-import ru.job4j.tracker.actions.DeleteAction;
-import ru.job4j.tracker.actions.EditAction;
+import ru.job4j.tracker.actions.*;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
@@ -12,35 +10,34 @@ import static org.junit.Assert.*;
 public class StartUITest {
 
     @Test
-    public void whenAdd() {
-        String[] answers = {"item_1"};
-        IInput input = new StubInput(answers);
+    public void whenCreateItem() {
+        String[] askedData = {"0", "item_1", "1"};
+        IInput stubInput = new StubInput(askedData);
+        IUserAction[] actions = {new CreateAction(), new ExitAction()};
         Tracker tracker = new Tracker();
-        new CreateAction().execute(input,tracker);
-        Item[] allItems = tracker.findAll();
-        assertThat(allItems[0].getName(), is("item_1"));
-    }
-
-    @Test
-    public void whenDeleteOnlyItem() {
-        Item item = new Item();
-        item.setName("item_1");
-        Tracker tracker = new Tracker();
-        tracker.add(item);
-        String[] answer = {String.valueOf(item.getId())};
-        IInput input = new StubInput(answer);
-        new DeleteAction().execute(input, tracker);
-        assertNull(tracker.findById(item.getId()));
+        new StartUI().init(stubInput, tracker,actions);
+        assertThat(tracker.findAll()[0].getName(), is("item_1"));
     }
 
     @Test
     public void replaceItemId1() {
-        Item item = new Item("item_1");
+        String[] askedData = {"0", "item_1", "1", "1", "item_item", "2"};
+        IInput stubInput = new StubInput(askedData);
         Tracker tracker = new Tracker();
-        tracker.add(item);
-        String[] answers = {String.valueOf(item.getId()), "item_item"};
-        IInput input = new StubInput(answers);
-        new EditAction().execute(input, tracker);
-        assertThat(tracker.findById(Integer.parseInt(answers[0])).getName(), is("item_item"));
+        IUserAction[] actions = {new CreateAction(), new EditAction(), new ExitAction()};
+        new StartUI().init(stubInput, tracker,actions);
+        assertThat(tracker.findAll()[0].getName(), is("item_item"));
     }
+
+    @Test
+    public void whenDeleteOnlyItem() {
+        String[] stubData = {"0", "item_1", "1", "1", "2"};
+        IInput stubInput = new StubInput(stubData);
+        IUserAction[] actions = {new CreateAction(), new DeleteAction(), new ExitAction()};
+        Tracker tracker = new Tracker();
+        new StartUI().init(stubInput, tracker, actions);
+        assertThat(tracker.findAll().length, is(0));
+    }
+
+
 }
