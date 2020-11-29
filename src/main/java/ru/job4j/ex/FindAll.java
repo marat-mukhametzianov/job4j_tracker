2f98b00ce06bb004b6f7578a76f967ad8d79296f
marat-mukhametzianov <marat.mukhametzianov@yahoo.com>
2020-11-29 16:02:13 -0500

    0.4. Пользовательские исключения. [#389693]

---
+++ src/main/java/ru/job4j/ex/FindAll.java
@@ -0,0 +1,32 @@
+package ru.job4j.ex;
+
+import java.util.Scanner;
+
+public class FindAll {
+
+    public static void main(String[] args) {
+        String[] strings = {"One", "Two"};
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a key word:");
+        String key = scanner.nextLine();
+        try {
+            int result = indexOf(strings, key);
+            System.out.println(result);
+        } catch (ElementNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int result = -1;
+        for(int i = 0; i < value.length; i++) {
+            if(value[i].equals(key)) {
+                result = i;
+            }
+        }
+        if (result == -1) {
+            throw new ElementNotFoundException("There is no such element in the given array.");
+        }
+        return result;
+    }
+}
