29783e7e2dd311d4a4ab9fba0647eed27af4a44a
marat-mukhametzianov <marat.mukhametzianov@yahoo.com>
2020-12-06 02:05:42 -0500

    2. Singleton. [#389707]

---
+++ src/main/java/ru/job4j/tracker/TrackerSingleFour.java
@@ -0,0 +1,78 @@
+package ru.job4j.tracker;
+
+import java.util.Arrays;
+
+public class TrackerSingleFour {
+    private final Item[] items = new Item[100];
+    private int ids = 1;
+    private int size = 0;
+
+    public static TrackerSingleFour getInstance() {
+        return TrackerSingleFourHolder.INSTANCE;
+    }
+
+    private TrackerSingleFour() { }
+
+    private static final class TrackerSingleFourHolder{
+        private static final TrackerSingleFour INSTANCE = new TrackerSingleFour();
+    }
+
+    public Item add(Item item) {
+        item.setId(ids++);
+        items[size++] = item;
+        return item;
+    }
+
+    public Item findById(int id) {
+        int index = indexOf(id);
+        return index != -1 ? items[index] : null;
+    }
+
+    /**
+     * The method returns a copy of the items array, but with no null elements
+     */
+    public Item[] findAll() {
+        return Arrays.copyOf(items, size);
+    }
+
+    public Item[] findByName(String key) {
+        Item[] result = new Item[size];
+        short index = 0;
+        for (int i = 0; i < size; i++) {
+            if (items[i].getName().equals(key)) {
+                result[index++] = items[i];
+            }
+        }
+        return Arrays.copyOf(result, index);
+    }
+
+    public boolean replace(int id, Item item) {
+        int index = indexOf(id);
+        boolean result = index != -1;
+        if (result) {
+            items[index] = item;
+            items[index].setId(id);
+        }
+        return result;
+    }
+
+    private int indexOf(int id) {
+        for (int i = 0; i < size; i++) {
+            if (items[i].getId() == id) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public boolean delete(int id) {
+        int index = indexOf(id);
+        boolean result = index != -1;
+        if (result) {
+            System.arraycopy(items, index + 1, items, index, size - index - 1);
+            items[size - 1] = null;
+            --size;
+        }
+        return result;
+    }
+}
---
+++ src/test/java/ru/job4j/tracker/TrackerSingleTest.java
@@ -0,0 +1,35 @@
+package ru.job4j.tracker;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TrackerSingleTest {
+    @Test
+    public void whenCreateSingletoneOne() {
+        TrackerSingleOne one = TrackerSingleOne.INSTANCE;
+        TrackerSingleOne two = TrackerSingleOne.INSTANCE;
+        assertSame(one, two);
+    }
+
+    @Test
+    public void whenCreateSingletoneTwo() {
+        TrackerSingleTwo one = TrackerSingleTwo.getINSTANCE();
+        TrackerSingleTwo two = TrackerSingleTwo.getINSTANCE();
+        assertSame(one, two);
+    }
+
+    @Test
+    public void whenCreateSingletoneThree() {
+        TrackerSingleThree one = TrackerSingleThree.getINSTANCE();
+        TrackerSingleThree two = TrackerSingleThree.getINSTANCE();
+        assertSame(one, two);
+    }
+
+    @Test
+    public void whenCreateSingletoneFour() {
+        TrackerSingleFour one = TrackerSingleFour.getInstance();
+        TrackerSingleFour two = TrackerSingleFour.getInstance();
+        assertSame(one, two);
+    }
+}
