197f06e1f9a244a891ac14dc6c4ec0ca54fc06ae
marat-mukhametzianov <marat.mukhametzianov@yahoo.com>
2020-12-20 19:47:57 -0500

    2. Очередь с приоритетом на LinkedList [#389651]

---
+++ src/main/java/ru/job4j/search/PriorityLine.java
@@ -0,0 +1,28 @@
+package ru.job4j.search;
+
+import java.util.LinkedList;
+
+public class PriorityLine {
+    private LinkedList<Task> tasks = new LinkedList<Task>();
+
+    /**
+     * The method has to place an element at specific position.
+     * The position is determined by the priority field.
+     * @param task is an object to add.
+     */
+    public void put (Task task) {
+        int index = 0;
+        for (Task item : tasks) {
+            if (task.getPriority() <= item.getPriority()) {
+                break;
+            }
+            index++;
+        }
+        this.tasks.add(index, task);
+    }
+
+    public Task take () {
+        return tasks.poll();
+    }
+
+}
---
+++ src/test/java/ru/job4j/search/PriorityLineTest.java
@@ -0,0 +1,19 @@
+package ru.job4j.search;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class PriorityLineTest {
+    @Test
+    public void whenTakeTheHighestPriority() {
+        PriorityLine priorityLine = new PriorityLine();
+        priorityLine.put(new Task("low", 5));
+        priorityLine.put(new Task("urgent", 2));
+        priorityLine.put(new Task("middle", 3));
+        Task takenTask = priorityLine.take();
+        assertThat(takenTask.getDescription(), is("urgent"));
+    }
+
+}
