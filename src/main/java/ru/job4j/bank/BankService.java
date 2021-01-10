1a9d2f3f5295620e7efc6727841f7dda61377707
marat-mukhametzianov <marat.mukhametzianov@yahoo.com>
2021-01-09 20:09:59 -0500

    3. Что такое JavaDoc [#389637]

--- src/main/java/ru/job4j/bank/BankService.java
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -5,13 +5,31 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * This class has three methods: creating a hash map based on a User as a key, and List of an
+ * Accounts, corresponding to this user.
+ * @author Marat. M.
+ * @version 1.0.
+ */
 public class BankService {
     private Map<User, List<Account>> users = new HashMap<>();
 
+    /**
+     * This method adds received instance of the User class into HashMap if there is no
+     * matching element. As a value is used an empty ArrayLi<Account>.
+     * @param user is an instance of the User class to be added into HashMap
+     */
     public void addUser(User user) {
         users.putIfAbsent(user, new ArrayList<Account>());
     }
 
+    /**
+     * The method adds a new account in case if there is no the same account belongs this user.
+     * Before adding it checks that the user with the received passport.
+     * Existence of the user with received passport is checked using the findByPassport() method.
+     * @param passport is a number to check the user existence.
+     * @param account is an instance of the class Account, passed to the user to add.
+     */
     public void addAccount(String passport, Account account) {
         User user = findByPassport(passport);
         if (user != null) {
@@ -22,6 +40,12 @@ public class BankService {
         }
     }
 
+    /**
+     * The method checks the HashMap in order to try to find a user with the received passport. If
+     * the user has been found then is returned.
+     * @param passport is a key to try to find a user.
+     * @return a value of either the found user or null.
+     */
     public User findByPassport(String passport) {
         for (User user : users.keySet()) {
             if (user.getPassport().equals(passport)) {
@@ -31,6 +55,14 @@ public class BankService {
         return null;
     }
 
+    /**
+     * The method tries to find a user using received passport. If the user is not a null then
+     * it checks user's account using requisite. If the account exists it is returned. Otherwise,
+     * null is returned.
+     * @param passport is a key to find a user.
+     * @param requisite is a number to identify an account.
+     * @return is either the found account or null.
+     */
     public Account findByRequisite(String passport, String requisite) {
         User user = findByPassport(passport);
         if (user != null) {
@@ -44,6 +76,16 @@ public class BankService {
         return null;
     }
 
+    /**
+     * The method performs a money transfer from one account to another, using their passports and
+     * requisites. The source is a giving side, the destination is receiving one.
+     * @param sourcePassport is a number to identify a giving user.
+     * @param sourceRequisite is a number to determine an account to transfer money from.
+     * @param destinationPassport is a number of receiving user.
+     * @param destinationRequisite is a number of an account of the receiving user.
+     * @param amount is a money quantity.
+     * @return is a state of a result of the transfer.
+     */
     public boolean moneyTransfer(String sourcePassport,
                                   String sourceRequisite,
                                   String destinationPassport,
--- src/main/java/ru/job4j/search/PriorityLine.java
+++ src/main/java/ru/job4j/search/PriorityLine.java
@@ -2,13 +2,20 @@ package ru.job4j.search;
 
 import java.util.LinkedList;
 
+/**
+ * This class designation is creating a LinkedList of Task elements, and taking its first element.
+ * @author Marat. M.
+ * @version 1.0.
+ */
 public class PriorityLine {
     private LinkedList<Task> tasks = new LinkedList<Task>();
 
     /**
-     * The method has to place an element at specific position.
-     * The position is determined by the priority field.
-     * @param task is an object to add.
+     * This method creates a LinkedList of Task elements according to their priorities.
+     * The priority of the received task is compared to the priority of each existing element.
+     * In case of the received task priority is less than the existing one, the value of the index
+     * is considered achieved. Then received task placed at the index position.
+     * @param task is an instance of the Task class, and is going to be added into LinkedList.
      */
     public void put(Task task) {
         int index = 0;
@@ -21,6 +28,10 @@ public class PriorityLine {
         this.tasks.add(index, task);
     }
 
+    /**
+     * The method returns the first element of the LinkedList, removing it from the list.
+     * @return is the first element of the LinkedList, or the null if the list is empty.
+     */
     public Task take() {
         return tasks.poll();
     }
