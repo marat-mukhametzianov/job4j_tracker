e27107a6b4eeb9986a8f18cb94045991538ddc94
marat-mukhametzianov <marat.mukhametzianov@yahoo.com>
2021-01-11 18:35:13 -0500

    2. Отсортировать департаменты [#389640]

--- src/main/java/ru/job4j/collection/departments/DescComparator.java
+++ src/main/java/ru/job4j/collection/departments/DescComparator.java
@@ -6,10 +6,6 @@ public class DescComparator implements Comparator<String> {
 
     @Override
     public int compare(String first, String second) {
-        if (first.split("/")[0].equals(second.split("/")[0])) {
-            return first.compareTo(second);
-        } else {
-            return second.compareTo(first);
-        }
+        return first.split("/")[0].equals(second.split("/")[0]) ? first.compareTo(second) :  second.compareTo(first);
     }
 }
