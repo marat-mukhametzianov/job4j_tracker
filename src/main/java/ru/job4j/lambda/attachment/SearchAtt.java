3f48f223f3167b1ba8083624c103a2c169e49ac5
marat-mukhametzianov <marat.mukhametzianov@yahoo.com>
2021-01-16 01:44:56 -0500

    2.0. Встроенные функциональные интерфейсы. [#389476]

--- src/main/java/ru/job4j/lambda/attachment/SearchAtt.java
+++ src/main/java/ru/job4j/lambda/attachment/SearchAtt.java
@@ -2,33 +2,40 @@ package ru.job4j.lambda.attachment;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Function;
+import java.util.function.Predicate;
 
 public class SearchAtt {
 
     public static List<Attachment> filterSize(List<Attachment> list) {
-        List<Attachment> result = new ArrayList<>();
-        for (Attachment attachment : list) {
-            if (attachment.getSize() > 100) {
-                result.add(attachment);
+        Predicate<Attachment> predicate = new Predicate<Attachment>() {
+            @Override
+            public boolean test(Attachment attachment) {
+                return attachment.getSize() > 100;
             }
-        }
-        return result;
+        };
+        return universalFilter(list, predicate);
     }
 
     public static List<Attachment> filterName(List<Attachment> list) {
+        Predicate<Attachment> predicate = new Predicate<Attachment>() {
+            @Override
+            public boolean test(Attachment attachment) {
+                return attachment.getName().contains("bug");
+            }
+        };
+        return universalFilter(list, predicate);
+    }
+
+    public static List<Attachment> universalFilter(
+            List<Attachment> attachments,
+            Predicate<Attachment> predicate
+    ) {
         List<Attachment> result = new ArrayList<>();
-        for (Attachment attachment : list) {
-            if (attachment.getName().contains("bug")) {
+        for (Attachment attachment : attachments) {
+            if (predicate.test(attachment)) {
                 result.add(attachment);
             }
         }
         return result;
-    }
-
-    public static List<Attachment> universalFilter(
-            List<Attachment> initialList,
-            Function <List<Attachment>, List<Attachment>> action) {
-        return action.apply(initialList);
     }
 }
--- src/test/java/ru/job4j/lambda/attachment/SearchAttTest.java
+++ src/test/java/ru/job4j/lambda/attachment/SearchAttTest.java
@@ -19,16 +19,7 @@ public class SearchAttTest {
                 new Attachment("Two", 100),
                 new Attachment("Three", 101)
         );
-        Function<List<Attachment>, List<Attachment>> name = x -> {
-            List<Attachment> result = new ArrayList<>();
-            for(Attachment attachment : x) {
-                if (attachment.getSize() > 100) {
-                    result.add(attachment);
-                }
-            }
-            return result;
-        };
-        List<Attachment> output = SearchAtt.universalFilter(input,name);
+        List<Attachment> output = SearchAtt.filterSize(input);
         assertThat(output.get(0).getName(), is("Three"));
     }
 
@@ -39,41 +30,7 @@ public class SearchAttTest {
                 new Attachment("bug", 100),
                 new Attachment("bug", 101)
         );
-        Function<List<Attachment>, List<Attachment>> name = x -> {
-            List<Attachment> result = new ArrayList<>();
-            for(Attachment attachment : x) {
-                if (attachment.getName().contains("bug")) {
-                    result.add(attachment);
-                }
-            }
-            return result;
-        };
-        List<Attachment> output = SearchAtt.universalFilter(input,name);
+        List<Attachment> output = SearchAtt.filterName(input);
         assertThat(output.size(), is(2));
-    }
-
-    @Test
-    public void simpleFilterSize() {
-        List<Attachment> input = Arrays.asList(
-                new Attachment("One", 50),
-                new Attachment("Two", 100),
-                new Attachment("Three", 101)
-        );
-        Function<List<Attachment>, List<Attachment>> name
-                =
-                new Function<List<Attachment>, List<Attachment>>() {
-            @Override
-            public List<Attachment> apply(List<Attachment> attachments) {
-                List<Attachment> result = new ArrayList<>();
-                for(Attachment attachment : attachments) {
-                    if (attachment.getSize() > 100) {
-                        result.add(attachment);
-                    }
-                }
-                return result;
-            }
-        };
-        List<Attachment> output = SearchAtt.universalFilter(input,name);
-        assertThat(output.get(0).getName(), is("Three"));
     }
 }
