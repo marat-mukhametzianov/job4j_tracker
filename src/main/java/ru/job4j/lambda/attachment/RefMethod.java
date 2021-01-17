b0a844843e24e431bbf0848303997fd5bb8a80ec
marat-mukhametzianov <marat.mukhametzianov@yahoo.com>
2021-01-17 01:35:53 -0500

    3. Ссылки на методы [#389471]

---
+++ src/main/java/ru/job4j/lambda/attachment/RefMethod.java
@@ -0,0 +1,21 @@
+package ru.job4j.lambda.attachment;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class RefMethod {
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList("0123456789", "01234567890123456789");
+        Consumer<String> rule = RefMethod::cutOff;
+        names.forEach(rule);
+    }
+
+    public static void cutOff(String initialString) {
+        if(initialString.length() > 10) {
+            System.out.println(initialString.substring(0, 10) + "...");
+        } else {
+            System.out.println(initialString);
+        }
+    }
+}
---
+++ src/main/java/ru/job4j/lambda/attachment/Test.java
@@ -0,0 +1,34 @@
+package ru.job4j.lambda.attachment;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+public class Test {
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList("One");
+        Comparator<String> comparator = (left, right) -> {
+            System.out.println("Lambda starts");
+            return left.compareTo(right);
+        };
+        names.sort(comparator);
+        names = Arrays.asList("One", "Two");
+        names.sort(comparator);
+        names.forEach(new Consumer<String>() {
+            @Override
+            public void accept(String s) {
+                System.out.println(s);
+            }
+        });
+        Function<String, Integer> instanceOfAnonymusClassImplementingInterface = new Function<String, Integer>() {
+            @Override
+            public Integer apply(String inletData) {
+                return Integer.parseInt(inletData);
+            }
+        };
+        System.out.println(instanceOfAnonymusClassImplementingInterface.apply("10") + 20);
+    }
+}
