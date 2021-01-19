70ac750bb461ce32f68bc99cdf518ddb16d3f091
marat-mukhametzianov <marat.mukhametzianov@yahoo.com>
2021-01-18 21:10:47 -0500

    5. Подсчет функции в диапазоне. [#389468]

---
+++ src/test/java/ru/job4j/lambda/SomeFunctionTest.java
@@ -0,0 +1,31 @@
+package ru.job4j.lambda;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class SomeFunctionTest {
+
+    @Test
+    public void linearMXPlusB() {
+        List<Double> result = new SomeFunction().range(5, 7, value -> 2 * value + 5);
+        assertThat(result, is(Arrays.asList(15D, 17D, 19D)));
+    }
+
+    @Test
+    public void quadraticMX2PlusB() {
+        List<Double> result = new SomeFunction().range(2, 4, value -> 2 * Math.pow(value, 2) + 5);
+        assertThat(result, is(Arrays.asList(13D, 23D, 37D)));
+    }
+
+    @Test
+    public void exponentialAPowX() {
+        List<Double> result = new SomeFunction().range(2, 3, value -> Math.pow(3, value));
+        assertThat(result, is(Arrays.asList(9D, 27D)));
+    }
+}
